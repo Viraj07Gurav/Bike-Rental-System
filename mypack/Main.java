1e6f5c2a4eb264a0caedd26227404fb4d0906c13
Viraj07Gurav <163198380+Viraj07Gurav@users.noreply.github.com>
2024-11-05 23:50:14 +0530

    Update Main.java

--- mypack/Main.java
+++ mypack/Main.java
@@ -3,7 +3,6 @@ package mypack;
 public class Main {
         public static void main(String[] args) {
            bikeRentalSystem rentalSystem = new bikeRentalSystem();
-    
             Bike bike1 = new Bike("C001", "Honda", "Splender", 60.0); 
             Bike bike2 = new Bike("C002", "Hero", "Activa", 70.0);
             Bike bike3 = new Bike("C003", "TVS", "Jupiter", 150.0);
